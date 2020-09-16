b0612e35d56ed8aaa7898ff3938ecaa2b28633a1
NIMO <laomms@gmail.com>
2020-09-16 16:54:17 +0700

    添加项目文件。

---
+++ test/test.cpp
@@ -0,0 +1,40 @@
+﻿#include "Windows.h"
+#include <iostream>
+#include <tchar.h>
+#include<cstdio>
+#include <unordered_map>
+using namespace std;
+
+
+typedef const char* (__stdcall *apprun)(const char* apidata, const char* pluginkey);
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	HMODULE hModule = LoadLibrary(L"D:\\DigitalAct\\DllUnload\\Debug\\DllUnload.dll");	
+	if (hModule != NULL)
+	{
+		apprun pAppRun = (apprun)GetProcAddress(hModule, "apprun");
+		const char* ss= pAppRun("123","456");
+		cout << ss << endl;
+		pAppRun = NULL;
+		ss = NULL;
+		FreeLibrary(hModule);
+	}    
+
+
+	const char* savePath = "D:\\DigitalAct\\DllUnload\\Debug\\DllUnload.dll";
+	int res = remove(savePath);
+	if (res == 0)
+	{
+		cout << "删除成功" << endl;
+	}
+	else
+	{
+		cout << "删除失败" << endl;
+	}
+	system("pause");
+	return 0;
+}
+
+
---
+++ testdll/Class1.cs
@@ -0,0 +1,41 @@
+﻿using RGiesecke.DllExport;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace testdll
+{
+    public class Class1
+    {
+		[DllExport(CallingConvention = CallingConvention.StdCall)]
+		//[return: MarshalAs(UnmanagedType.LPStr)]
+		public static string apprun([MarshalAs(UnmanagedType.LPStr)] string apidata, [MarshalAs(UnmanagedType.LPStr)] string pluginkey)
+		{
+			return "abc";
+		}
+
+  //      //c# 加载卸载模式
+		//public static void LoadDll(string apidata, string pluginkey)
+  //      {
+  //          AppDomain ad = AppDomain.CreateDomain("Isolate DLL");
+  //          Assembly a = ad.Load(new AssemblyName("testdll"));
+  //          object d = a.CreateInstance("testdll.Class1");
+  //          Type t = d.GetType();
+  //          string result = (string)t.InvokeMember("apprun", BindingFlags.InvokeMethod, null, d, new object[] { apidata, pluginkey });
+  //          AppDomain.Unload(ad);
+  //      }
+  //      public static void UnloadDll(string apidata, string pluginkey)
+  //      {
+  //          AppDomain ad = AppDomain.CreateDomain("Isolate DLL");
+  //          Assembly a = ad.Load(new AssemblyName("testdll"));
+  //          object d = a.CreateInstance("testdll.Class1");
+  //          Type t = d.GetType();
+  //          string result = (string)t.InvokeMember("apprun", BindingFlags.InvokeMethod, null, d, new object[] { apidata, pluginkey });
+  //          AppDomain.Unload(ad);
+  //      }
+    }
+}
